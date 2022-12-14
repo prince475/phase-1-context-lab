0bf94af31aab83c6682e19a7f8049e40d9eacad3
prince475 <danielmwai475@gmail.com>
2022-12-14 07:29:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 /* Your Code Here */
+// Loads Array elements into corresponding Object properties
+function createEmployeeRecord(obj) {
+    const filledArray = {
+        firstName: obj[0],
+        familyName: obj[1],
+        title: obj[2],
+        payPerHour: obj[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return filledArray
+}
+
+//Converts each nested Array into an employee record using createEmployeeRecord and accumulates it to a new Array
+function createEmployeeRecords(array){
+    return array.map((obj) => createEmployeeRecord(obj))
+}
+
+function createTimeInEvent(timeStamp){
+    const [date, hour] = timeStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: Number(hour),
+        date: date
+    })
+    return this
+}
+
+function createTimeOutEvent(timeStamp){
+    const [date, hour] = timeStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: Number(hour),
+        date: date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(timeStamp) {
+    const timeIn = this.timeInEvents.find(event => event.date === timeStamp)
+
+    const timeOut = this.timeOutEvents.find(event => event.date === timeStamp)
+
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(hours) {
+    const wages = hoursWorkedOnDate.call(this, hours)
+        * this.payPerHour
+    return Number(wages)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +75,11 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(collection, firstName) {
+    return collection.find(record => record.firstName === firstName)
+}
+
+function calculatePayroll(recordsArray){
+    return recordsArray.reduce((employee, record) => employee + allWagesFor.call(record), 0)
+
+}
